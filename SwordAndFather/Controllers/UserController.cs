5282fcd88a8fdd6fe2a9271455afda5b51e62921
Maggie Leavell <mjleavell@gmail.com>
2019-03-26 21:03:21 -0500

    api running in postmat

---
+++ SwordAndFather/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SwordAndFather.Models;
+
+namespace SwordAndFather.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        // instatiated list of new users
+        List<User> _users = new List<User>();
+
+        [HttpPost("register")]
+        public int AddUser(string username, string password)
+        {
+            var newUser = new User(username, password);
+
+            newUser.Id = _users.Count + 1;
+
+            _users.Add(newUser);
+
+            return newUser.Id;
+        }
+    }
+}
