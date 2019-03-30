92a67b1f5da71d8f316e796449fffcee59a7c933
Maggie Leavell <mjleavell@gmail.com>
2019-03-30 10:18:20 -0500

    moved UserRepo and Validator to separate files

--- SwordAndFather/Controllers/UsersController.cs
+++ SwordAndFather/Controllers/UsersController.cs
@@ -4,49 +4,45 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SwordAndFather.Data;
 using SwordAndFather.Models;
 
 namespace SwordAndFather.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class UserController : ControllerBase
+    public class UsersController : ControllerBase
     {
-        // instatiated list of new users
-        List<User> _users = new List<User>();
+        // fields
+        readonly UserRepository _userRepository;
+        readonly CreateUserRequestValidator _validator;
+
+        // property
+        public UsersController()
+        {
+            var _validator = new CreateUserRequestValidator();
+            var _userRepository = new UserRepository();
+        }
+
 
         [HttpPost("register")]
         // asp.net is creating user for us.
-        // Bad Request and Crated methods are getting inherited from the base class
-        // ActionResult doesnt necessarily need a type
+        // Bad Request and Crated methods are getting inherited from the base class; ActionResult doesnt necessarily need a type
         //public ActionResult<int> AddUser(User newUser)
         public ActionResult<int> AddUser(CreateUserRequest createRequest)
         {
-            // ****validation****
-            if (string.IsNullOrEmpty(createRequest.Username) && string.IsNullOrEmpty(createRequest.Password))
+
+            if (!_validator.Validate(createRequest))
             {
-                // below is how you create a new anonymous type
-                // have to return bad request in order for it to actually be seen
+                // below is how you create a new anonymous type; have to return bad request in order for it to actually be seen
                 return BadRequest(new { error = "users must have a username and password" });
             }
 
-            // ****database-y sort of thing****
-            var newUser = new User(createRequest.Username, createRequest.Password);
-
-            newUser.Id = _users.Count + 1;
-
-            _users.Add(newUser);
-
-            //return newUser.Id;
+            var newUser = _userRepository.AddUser(createRequest.Username, createRequest.Password);
 
             // ****HTTP Response****
             // Created method is a method that gives us 201
             return Created($"api/users/{newUser.Id}", newUser);
         }
-    }
-
-    public class createUserRequestValidator
-    {
-
     }
 }
---
+++ SwordAndFather/Data/UserRepository.cs
@@ -0,0 +1,29 @@
+﻿using SwordAndFather.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SwordAndFather.Data
+{
+    public class UserRepository
+    {
+        // static modifier (usually avoid unless absolutely necessary): The static modifier makes an item non-instantiable, 
+        // it means the static item cannot be instantiated. 
+        // If the static modifier is applied to a class then that class cannot be instantiated using the new keyword. 
+        // If the static modifier is applied to a variable, 
+        // method or property of class then they can be accessed without creating an object of the class
+        static List<User> _users = new List<User>();
+
+        public User AddUser(string username, string password)
+        {
+            var newUser = new User(username, password);
+
+            newUser.Id = _users.Count + 1;
+
+            _users.Add(newUser);
+
+            return newUser;
+        }
+    }
+}
